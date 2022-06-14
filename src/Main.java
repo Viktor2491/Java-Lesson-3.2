201c4bc82ec0c258909b16fa195edf3267521525
viktor91 <v.a.taradaiko@gmail.com>
2022-06-14 09:24:05 +0400

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
-        int payment = 100;
-        int bonus;
+        int balance = 100; // Первоначальный баланс
+        int payment = 100; // Платеж
+        int bonus; // Бонусные рубли
         if (payment >= 1000) {
             bonus = payment / 100 * 1;
         } else {
             bonus = 0;
         }
-        int finalbalance = balance + payment + bonus;
+        int finalbalance = balance + payment + bonus; // Итоговый баланс
         System.out.println("Начислено бонусных рублей:");
         System.out.println(bonus);
         System.out.println("Итоговый баланс:");
